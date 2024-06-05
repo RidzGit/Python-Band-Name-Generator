f0637b4ec06f23d383889b7b20382b160d7f430f
RidzGit <146337899+RidzGit@users.noreply.github.com>
2024-06-05 23:38:49 +0530

    Add files via upload

---
+++ Band_name_gen.py
@@ -0,0 +1,11 @@
+print("Welcome to the Band Name Generator!")
+print("What city did you grow up in?")
+city = input()
+print("What is your pet's name?")
+pet = input()
+print ("Your band name could be : ", city + " " + pet)
+
+""" numchar = len(input("What city did you grow up in?"))
+print(type(numchar))
+new_numchar = str(numchar)
+print("The name has " + new_numchar + " characters") """
